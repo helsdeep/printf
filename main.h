6eb07be7245adad8d9760169912b878e15cc7a4e
helsdeep <femiossai@outlook.com>
2022-08-09 10:57:27 +0000

    fixed errors

--- main.h
+++ main.h
@@ -5,12 +5,12 @@
 #include <stdarg.h>
 
 /**
-*struct flags - struct containing flags to "turn on"
-*when a flag specifier is passed to _printf()
-*@plus: flag for the '+' character
-*@space: flag for the ' ' character
-*@hash: flag for the '#' character
-*/
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
 typedef struct flags
 {
 	int plus;
@@ -19,11 +19,11 @@ typedef struct flags
 } flags_t;
 
 /**
-*struct printHandler - struct to choose the right function depending
-*on the format specifier passed to _printf()
-*@c: format specifier
-*@f: pointer to the correct printing function
-*/
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
 typedef struct printHandler
 {
 	char c;
